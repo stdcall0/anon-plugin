b95cda7aa2c8e74ce66efee36bf6e398d46fd49d
actions-user <action@github.com>
2025-06-22 13:46:22 +0000

    TS Build: 0af9409a3553b0937bbe4e1a39e4818716d213bc

--- apps/quotes.js
+++ apps/quotes.js
@@ -45,7 +45,7 @@ export class QuotesPlugin extends Plugin {
         return s.slice(index + kw.length).split(' ').filter(tag => tag);
     }
     setId(message_ids, quote_id) {
-        message_ids.forEach(m => 
+        message_ids.forEach(async (m) => 
         // @ts-ignore
         await redis.set(`${PLUGIN_ID}:quotes:id:${m}`, quote_id.toString(), { EX: 3600 * 3 }));
     }
